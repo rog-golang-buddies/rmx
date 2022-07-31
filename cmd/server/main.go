670b510327ee7c63c6c877c078623323cd7093c3
Parham Moieni <pmoieni06@gmail.com>
2022-07-31 23:57:31 +0430

    replaced ent with sqlc

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"context"
 	"log"
 
-	_ "github.com/go-sql-driver/mysql"
+	// _ "github.com/go-sql-driver/mysql"
 	rmx "github.com/rog-golang-buddies/rapidmidiex"
-	"github.com/rog-golang-buddies/rapidmidiex/ent"
-	"github.com/spf13/viper"
 )
 
 func main() {
@@ -17,23 +14,18 @@ func main() {
 		log.Fatalf("fatal error config file: %v", err.Error())
 	}
 
-	dsn := viper.GetString("DB_USER") + ":" +
-		viper.GetString("DB_PASSWORD") + "@tcp(" +
-		viper.GetString("DB_HOST") + ":" +
-		viper.GetString("DB_PORT") + ")/" +
-		viper.GetString("DB_NAME") + "?parseTime=True"
-
-	log.Println("opening db connection")
-	client, err := ent.Open("mysql", dsn)
-	if err != nil {
-		log.Fatalf("failed opening connection to mysql: %v", err)
-	}
-	defer client.Close()
-	// Run the auto migration tool.
-	log.Println("running auto migration")
-	if err := client.Schema.Create(context.Background()); err != nil {
-		log.Fatalf("failed creating schema resources: %v", err)
-	}
+	// database connection shouldn't be implemented here
+	// 	dsn := viper.GetString("DB_USER") + ":" +
+	// 		viper.GetString("DB_PASSWORD") + "@tcp(" +
+	// 		viper.GetString("DB_HOST") + ":" +
+	// 		viper.GetString("DB_PORT") + ")/" +
+	// 		viper.GetString("DB_NAME") + "?parseTime=True"
+	//
+	// 	log.Println("opening db connection")
+	// 	db, err := sql.Open("mysql", dsn)
+	// 	if err != nil {
+	// 		log.Fatalf("database connectoin failed: %v", err.Error())
+	// 	}
 
 	s := rmx.Server{
 		Port: ":8080",
