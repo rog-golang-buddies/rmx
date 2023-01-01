a5e4dfa8a464f8a448b4687efcfb56d181d60800
aboublef <kristopherab@gmail.com>
2023-01-01 07:06:07 +0000

    fixed testing

--- pkg/auth/auth_test.go
+++ pkg/auth/auth_test.go
@@ -1,9 +1,11 @@
-package auth
+package auth_test
 
 import (
 	"net/http"
 	"testing"
 	"time"
+
+	"github.com/rog-golang-buddies/rmx/pkg/auth"
 
 	"github.com/hyphengolang/prelude/testing/is"
 	"github.com/hyphengolang/prelude/types/email"
@@ -11,55 +13,59 @@ import (
 	"github.com/hyphengolang/prelude/types/suid"
 	"github.com/lestrrat-go/jwx/v2/jwa"
 	"github.com/lestrrat-go/jwx/v2/jwt"
-	"github.com/rog-golang-buddies/rmx/internal"
 )
 
+type TestUser struct {
+	ID       suid.UUID
+	Username string
+	Email    email.Email
+	Password password.PasswordHash
+}
+
 func TestToken(t *testing.T) {
-	t.Parallel()
 	is := is.New(t)
 
 	t.Run(`generate a token and sign`, func(t *testing.T) {
-		_, private := ES256()
+		_, private := auth.ES256()
 
-		u := internal.User{
+		u := TestUser{
 			ID:       suid.NewUUID(),
 			Username: "fizz_user",
 			Email:    "fizz@mail.com",
 			Password: password.Password("492045rf-vf").MustHash(),
 		}
 
-		o := TokenOption{
+		o := auth.TokenOption{
 			Issuer:     "github.com/rog-golang-buddies/rmx",
 			Subject:    suid.NewUUID().String(),
 			Expiration: time.Hour * 10,
 			Claims:     map[string]any{"email": u.Email},
 		}
 
-		_, err := Sign(private, &o)
+		_, err := auth.Sign(private, &o)
 		is.NoErr(err) // sign id token
 
 		o.Subject = u.ID.String()
-		o.Expiration = AccessTokenExpiry
+		o.Expiration = auth.AccessTokenExpiry
 
-		_, err = Sign(private, &o)
+		_, err = auth.Sign(private, &o)
 		is.NoErr(err) // access token
 
-		o.Expiration = RefreshTokenExpiry
-		_, err = Sign(private, &o)
+		o.Expiration = auth.RefreshTokenExpiry
+		_, err = auth.Sign(private, &o)
 		is.NoErr(err) // refresh token
 	})
 }
 
 func TestMiddleware(t *testing.T) {
-	t.Parallel()
 	is := is.New(t)
 
 	t.Run("jwk parse request", func(t *testing.T) {
-		public, private := ES256()
+		public, private := auth.ES256()
 
 		e, cookieName := email.Email("foobar@gmail.com"), `__g`
 
-		o := TokenOption{
+		o := auth.TokenOption{
 			Issuer:     "github.com/rog-golang-buddies/rmx",
 			Subject:    suid.NewUUID().String(),
 			Expiration: time.Hour * 10,
@@ -67,7 +73,7 @@ func TestMiddleware(t *testing.T) {
 		}
 
 		// rts
-		rts, err := Sign(private, &o)
+		rts, err := auth.Sign(private, &o)
 		is.NoErr(err) // signing refresh token
 
 		c := &http.Cookie{
@@ -81,7 +87,6 @@ func TestMiddleware(t *testing.T) {
 		req, _ := http.NewRequest(http.MethodGet, "/", nil)
 		req.AddCookie(c)
 
-		//
 		_, err = jwt.Parse([]byte(c.Value), jwt.WithKey(jwa.ES256, public), jwt.WithValidate(true))
 		is.NoErr(err) // parsing jwk page not found
 	})
