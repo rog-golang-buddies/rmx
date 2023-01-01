38c5e666244b058f39bc44122e7a7e739a393b4c
aboublef <kristopherab@gmail.com>
2023-01-01 14:05:39 +0000

    removed not compliant packages

--- services/auth/postgres/init/init.sql
+++
@@ -1,9 +0,0 @@
-create extension if not exists "citext";
--- using Serial https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-serial/
-create table if not exists "credentials" (
-    id smallserial,
-    email citext unique not null check (email ~ '^[a-zA-Z0-9.!#$%&`’*+/=?^_\x60{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$'),
-    password citext not null check (password <> ''),
-    created_at timestamp not null default now()
-);
-
--- terminal/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/rog-golang-buddies/rmx/ui/terminal/tui"
-
-func main() {
-	tui.Run()
-}
